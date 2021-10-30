96c0b0c6326241da84ce81fa6cbc215672ef1b36
Justim Odendaal <justim.odendaal@gmail.com>
2021-10-30 12:47:56 +0100

    Added Type Convertor Exntension Methods and tests

---
+++ src/GraphQL.SQL/Convertors/GraphQLTypeConvertorExtensions.cs
@@ -0,0 +1,52 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+
+namespace GraphQL.SQL.Convertors
+{
+    public static class GraphQLTypeConvertorExtensions
+    {
+        private static Dictionary<string, Type> _sqlToGraphQLTypeMappings = new Dictionary<string, Type>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "varbinary", typeof(ByteGraphType) },
+            { "binary", typeof(ByteGraphType) },
+            { "timestamp", typeof(ByteGraphType) },
+            { "image", typeof(ByteGraphType) },
+            { "date", typeof(DateGraphType) },
+            { "time", typeof(DateTimeGraphType) },
+            { "datetime", typeof(DateTimeGraphType) },
+            { "datetime2", typeof(DateTimeGraphType) },
+            { "smalldatetime", typeof(DateTimeGraphType) },
+            { "datetimeoffset", typeof(DateTimeGraphType) },
+            { "bit", typeof(BooleanGraphType) },
+            { "uniqueidentifier", typeof(GuidGraphType) },
+            { "varchar", typeof(StringGraphType) },
+            { "nchar", typeof(StringGraphType) },
+            { "nvarchar", typeof(StringGraphType) },
+            { "text", typeof(StringGraphType) },
+            { "ntext", typeof(StringGraphType) },
+            { "char", typeof(StringGraphType) },
+            { "xml", typeof(StringGraphType) },
+            { "float", typeof(FloatGraphType) },
+            { "real", typeof(FloatGraphType) },
+            { "decimal", typeof(DecimalGraphType) },
+            { "numeric", typeof(DecimalGraphType) },
+            { "money", typeof(DecimalGraphType) },
+            { "smallmoney", typeof(DecimalGraphType) },
+            { "bigint", typeof(BigIntGraphType) },
+            { "int", typeof(IntGraphType) },
+            { "tinyint", typeof(IntGraphType) },
+            { "smallint", typeof(IntGraphType) }
+        };
+
+        public static Type ToGraphQLType(this string dbType)
+        {
+            if (_sqlToGraphQLTypeMappings.ContainsKey(dbType))
+            {
+                return _sqlToGraphQLTypeMappings[dbType];
+            }
+
+            throw new NotSupportedException($"{dbType} is not supported at present as a GraphQL Type");
+        }
+    }
+}
---
+++ src/GraphQL.SQL/Convertors/SqlDbTypeConvertorExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace GraphQL.SQL.Convertors
+{
+    public static class SqlDbTypeConvertorExtensions
+    {
+        private static Dictionary<string, SqlDbType>  _stringToSqlDbType = CreateSqlDbTypeMapping();
+
+        private static Dictionary<string, SqlDbType> CreateSqlDbTypeMapping()
+        {
+            var typeMappings = new Dictionary<string, SqlDbType>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (SqlDbType t in Enum.GetValues(typeof(SqlDbType)))
+            {
+                typeMappings.Add(t.ToString().ToLowerInvariant(), t);
+            }
+
+            typeMappings.Add("numeric", SqlDbType.Decimal);
+
+            return typeMappings;
+        }
+
+        public static SqlDbType ToSqlDbType(this string type)
+        {
+            if(_stringToSqlDbType.Count == 0)
+            {
+                CreateSqlDbTypeMapping();
+            }
+
+            if (_stringToSqlDbType.ContainsKey(type))
+            {
+                return _stringToSqlDbType[type];
+            }
+            
+            throw new NotSupportedException($"{type} is not supported at present as a SqlDbType");
+        }
+    }
+}
